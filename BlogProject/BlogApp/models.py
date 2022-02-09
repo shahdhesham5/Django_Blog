9f963dcb2f640929ea14790ee1853ddf25685298
rana <ranawael9721@gmail.com>
2022-02-09 23:48:49 +0200

    addinf commnt model

--- BlogProject/BlogApp/models.py
+++ BlogProject/BlogApp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User #User model to access users and admins
 
-# Create your models here.
+class Comment(models.Model):
+    comment_info = models.CharField(max_length=250)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # post = models.ForeignKey(User, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.comment_info
+
