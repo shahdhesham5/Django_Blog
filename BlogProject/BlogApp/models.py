5e05922850ebc6bc9e680d24b7c55dc848beb6b7
sohila-mo <sohilamhmmd@gmail.com>
2022-02-11 13:01:44 -0800

    commit

--- BlogProject/BlogApp/models.py
+++ BlogProject/BlogApp/models.py
@@ -7,6 +7,8 @@ class Category(models.Model):
     def __str__(self):
         return self.category
 
+
+
 class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length = 50)
